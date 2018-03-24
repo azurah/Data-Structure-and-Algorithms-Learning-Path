f8227da046299e1fb6548942a65fd5729838e433
Vivek <vivekkumar.be01@gmail.com>
2018-03-25 00:59:15 +0530

    update

--- Codes/Cpp/Day0_Q1_c.cpp
+++ Codes/Cpp/Day0_Q1_c.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 // // Question 1 part c
 void q1c(){
+  
 	for (int i = 0; i < 5; ++i){
-		for (int p = 0; p < 2*(4-i); ++p){
+		for (int p = 0; p < 2*(4-i); ++p){  //loop for padding
 			cout<<" ";
 		}
-		for (int j = i+1; j <= 2*i+1; ++j){
+		for (int j = i+1; j <= 2*i+1; ++j){ //loop to print first triangle elements
 			cout<<j<<" ";
 		}
-		for (int k = 2*i;k >=i+1; k--){
+		for (int k = 2*i;k >=i+1; k--){     //loop to print second triangle elements
 			cout<<k<<" ";
 		}		
 		cout<<endl;
 	}
+  
 }
 // Main function
 int main(){
